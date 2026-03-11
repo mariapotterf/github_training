b14f44484b82ff446634d352bca42927d46d71e0
Potterf Mária <potterf@fld.czu.cz>
2026-03-11 16:44:15 +0100

    exported plot

--- 01_read_data_make_plot.R
+++ 01_read_data_make_plot.R
@@ -9,7 +9,13 @@ dat <- read.csv("rawData/dat25_subplot_recode.csv")
 summary(dat)
 View(dat)
 
-dat %>% 
+p_box <- dat %>% 
   filter(hgt_est < 2) %>% 
   ggplot(aes(hgt_est)) +
   geom_boxplot()
+
+
+p_box
+
+ggsave('outFigs/p_box.png',
+       p_box)
