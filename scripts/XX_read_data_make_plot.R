c36700855d919f24774e92993e4935970a937fbd
Potterf Mária <potterf@fld.czu.cz>
2026-03-12 14:01:06 +0100

    moved the scripts within the script folder

---
+++ scripts/XX_read_data_make_plot.R
@@ -0,0 +1,33 @@
+
+
+# Read my data and make plots
+library(ggplot2)
+library(dplyr)
+
+#dat <- read.csv("rawData/dat25_subplot_recode.csv")
+
+dat <-"../rawData/dat25_subplot_recode.csv"
+
+# scripts
+#dat <- read.csv("../rawData/dat25_subplot_recode.csv")
+
+
+summary(dat)
+View(dat)
+
+p_box <- dat %>% 
+  filter(hgt_est < 2) %>% 
+  ggplot(aes(hgt_est)) +
+  geom_boxplot()
+
+
+p_box
+
+
+p_box2 <- p_box
+
+p_box3 <- p_box2
+
+
+ggsave('outFigs/p_box.png',
+       p_box)
